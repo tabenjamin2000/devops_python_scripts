372967e974f0223814740744a0c0032c9f8c92c9
Benjamin <tabenjamin2000@yahoo.fr>
2022-11-20 01:20:05 -0500

    email status check

---
+++ email_status_check.py
@@ -0,0 +1,14 @@
+### A python code that takes user email address as input and checks it then tells the user if the email is valid or not.
+### Prerequesite - pip3 install email_validator
+
+from email_validator import validate_email, EmailNotValidError
+
+def verify_status(email):
+	try:
+		_status = validate_email(email)
+		print("Success!! You have entered a valid email")
+	except EmailNotValidError as error_message:
+		print(str(error_message))
+
+_email = input("Please enter your email: ")
+verify_status(_email)
